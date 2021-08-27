ed29442468d6ec77299fb0a00442263cfa1c5022
Bogdan Artyushenko <Bogdan.Artyushenko@gmail.com>
2021-08-27 10:20:21 +0300

    vehicle dob

--- valtabl-pr.js
+++ valtabl-pr.js
@@ -1,10 +1,12 @@
 
 function getRandomState() {
+  let dob = new Date((Date.now() - 10 * 365 * 24 * 60 * 60 * 1000) * Math.random());
   return {
     "land-kadastr": make_digits(12) + ':' + make_digits(3) + ':' + make_digits(4),
     "neruhome-reiestr-nomer": make_digits(13),
     "neruhome-nomer-vlasnosti": make_digits(9),
     "neruhome-data-vlasnosti": toUkrDateTime(new Date(Math.round(Date.now() - 10 * 365 * 24 * 60 * 60 * 1000 * Math.random()))),
+    "transport-dov": dob.toISOString().substr(0, 10),
     "transport-nomernyi-znak": make_transp_num(),
     "transport-vin": make_vin()
   };
@@ -44,7 +46,15 @@ class LEValuesTable extends React.Component {
       className: "neruhome-nomer-vlasnosti"
     }, this.state["neruhome-nomer-vlasnosti"]))), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, "\u043D\u0435\u0440\u0443\u0445\u043E\u043C\u0435 \u043C\u0430\u0439\u043D\u043E"), /*#__PURE__*/React.createElement("td", null, "\u0434\u0430\u0442\u0430, \u0447\u0430\u0441 \u0434\u0435\u0440\u0436\u0430\u0432\u043D\u043E\u0457 \u0440\u0435\u0454\u0441\u0442\u0440\u0430\u0446\u0456\u0457"), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("span", {
       className: "neruhome-data-vlasnosti"
-    }, this.state["neruhome-data-vlasnosti"]))), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, "\u0442\u0440\u0430\u043D\u0441\u043F\u043E\u0440\u0442\u043D\u0438\u0439 \u0437\u0430\u0441\u0456\u0431"), /*#__PURE__*/React.createElement("td", null, "\u0456\u0434\u0435\u043D\u0442\u0438\u0444\u0456\u043A\u0430\u0446\u0456\u0439\u043D\u0438\u0439 \u043D\u043E\u043C\u0435\u0440 \u0442\u0440\u0430\u043D\u0441\u043F\u043E\u0440\u0442\u043D\u043E\u0433\u043E \u0437\u0430\u0441\u043E\u0431\u0443 (VIN)"), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("span", {
+    }, this.state["neruhome-data-vlasnosti"]))), 
+
+    React.createElement("tr", null, 
+                  React.createElement("td", null, "транспортний засіб"), 
+                  React.createElement("td", null, "дата будівництва"), 
+                  React.createElement("td", null, React.createElement("span", {className: "transport-dov"}, this.state["transport-dov"])
+    )),
+
+/*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, "\u0442\u0440\u0430\u043D\u0441\u043F\u043E\u0440\u0442\u043D\u0438\u0439 \u0437\u0430\u0441\u0456\u0431"), /*#__PURE__*/React.createElement("td", null, "\u0456\u0434\u0435\u043D\u0442\u0438\u0444\u0456\u043A\u0430\u0446\u0456\u0439\u043D\u0438\u0439 \u043D\u043E\u043C\u0435\u0440 \u0442\u0440\u0430\u043D\u0441\u043F\u043E\u0440\u0442\u043D\u043E\u0433\u043E \u0437\u0430\u0441\u043E\u0431\u0443 (VIN)"), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("span", {
       className: "transport-vin"
     }, this.state["transport-vin"]))), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, "\u0442\u0440\u0430\u043D\u0441\u043F\u043E\u0440\u0442\u043D\u0438\u0439 \u0437\u0430\u0441\u0456\u0431"), /*#__PURE__*/React.createElement("td", null, "\u0440\u0435\u0454\u0441\u0442\u0440\u0430\u0446\u0456\u0439\u043D\u0438\u0439 \u0437\u043D\u0430\u043A \u0442\u0440\u0430\u043D\u0441\u043F\u043E\u0440\u0442\u043D\u043E\u0433\u043E \u0437\u0430\u0441\u043E\u0431\u0443"), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("span", {
       className: "transport-nomernyi-znak"
