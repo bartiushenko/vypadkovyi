8441ad2638d763bfa44bf50f428464e7a3c0730e
Bogdan Artyushenko <bogdartysh@users.noreply.github.com>
2021-09-24 13:50:43 +0300

    Update valtabl-pe-passport.js

--- valtabl-pe-passport.js
+++ valtabl-pe-passport.js
@@ -85,7 +85,7 @@ class ValuesTable extends React.Component {
         width: "33%"
       }
     })), React.createElement("thead", null, React.createElement("tr", null, React.createElement("th", null, "\u0411\u041B\u041E\u041A"), React.createElement("th", null, "\u041A\u041B\u042E\u0427"), React.createElement("th", null, "\u0417\u041D\u0410\u0427\u0415\u041D\u041D\u042F ", React.createElement("button", {
-      onClick: this.nextRandomState,  className: "col-sm-2  btn-primary mb-2"
+      onClick: this.nextRandomState,  className: "btn-primary mb-2"
     }, "оновити")))), React.createElement("tbody", null, React.createElement("tr", null, React.createElement("td", null, "\u043F\u043E\u0434\u0430\u0442\u043A\u043E\u0432\u0430 \u0456\u043D\u0444\u043E"), React.createElement("td", null, "\u0440\u0435\u0454\u0441\u0442\u0440\u0430\u0446\u0456\u0439\u043D\u0438\u0439 \u043D\u043E\u043C\u0435\u0440 \u043E\u0431\u043B\u0456\u043A\u043E\u0432\u043E\u0457 \u043A\u0430\u0440\u0442\u043A\u0438 \u043F\u043B\u0430\u0442\u043D\u0438\u043A\u0430 \u043F\u043E\u0434\u0430\u0442\u043A\u0456\u0432"), React.createElement("td", null, React.createElement("span", {
       className: "podatkova-taxcode"
     }, this.state["podatkova-taxcode"]))),
